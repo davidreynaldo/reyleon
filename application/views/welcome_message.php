6f0f5db1bb1339a4f018afe6dc1c9df5853289c4
primerRepo <reynaldochaveztorrez@gmail.com>
2022-03-09 21:15:17 -0400

    Se agrega otra modificacion en el main

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>HOLA MUNDO BIEN VENIDO A OTRA PRUEBA DE MODIFICACION JEJJEJE</title>
 
 	<style type="text/css">
 
